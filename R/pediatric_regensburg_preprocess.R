9c876b6f84c49b90e58089dd9ffae9f4a7bb690e
MSimnach <marco.simnacher@gmail.com>
2024-04-11 18:36:48 +0200

    function for pediatric dataset

--- R/pediatric_regensburg_preprocess.R
+++ R/pediatric_regensburg_preprocess.R
@@ -1,4 +1,14 @@
+#' Pediatric_regensburg_preprocess
+#'
+#' Prepare the pediatric_regensburg dataset for the DNCIT package
+#'
+#' @return
+#' @export
+#'
+construct_dataset_pediatric_regensburg <- function() {
+  # Download the dataset from Zenodo
+  zenodo <- zen4R::ZenodoManager$new(logger = "INFO")
+  rec <- zenodo$getRecordByDOI("10.5281/zenodo.7669442")
+  files <- rec$listFiles(pretty = TRUE)
+}
 
-zenodo <- zen4R::ZenodoManager$new(logger = "INFO")
-rec <- zenodo$getRecordByDOI("10.5281/zenodo.7669442")
-files <- rec$listFiles(pretty = TRUE)
